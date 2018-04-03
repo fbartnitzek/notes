f896b85f37ab80993ccdbceeb06233e731dad2a0
fbartnitzek <frank_bartnitzek@web.de>
2018-04-03 17:25:14 +0200

    added associations-examples and modules-example and updated yelpCamp - now yelpCamp has comments and seed-file, but comments cannot be added

--- web_developer_bootcamp/8_yelpCamp/YelpCamp/app.js
+++ web_developer_bootcamp/8_yelpCamp/YelpCamp/app.js
@@ -1,19 +1,13 @@
 var express         = require("express"),
     app             = express(),
     bodyParser      = require("body-parser"),
-    mongoose        = require("mongoose");
-
+    mongoose        = require("mongoose"),
+    Campground      = require("./models/campground"),
+    seedDB          = require("./seeds");
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-
-// SCHEMA setup
-var campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-var Campground = mongoose.model("Campground", campgroundSchema);
+seedDB();
 
 app.get("/", function (req, res) {
     res.render("landing");
@@ -56,10 +50,11 @@ app.get("/campgrounds/new", function(req, res){
 
 // SHOW - more infos about one campground
 app.get("/campgrounds/:id", function(req, res){
-    Campground.findById(req.params.id, function(err, foundCampground){
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
        if (err){
            console.log(err);
        } else {
+           console.log(foundCampground);
            res.render("show", {campground: foundCampground});
        }
     });
